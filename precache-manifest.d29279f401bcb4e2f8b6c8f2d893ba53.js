7232a0cf4217d085831739c8103dfcd6b6230a9a
Fernanda Alves <fernanda__m@hotmail.com>
2020-03-18 14:03:15 -0300

    Updates

---
+++ precache-manifest.d29279f401bcb4e2f8b6c8f2d893ba53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e7e72082bf74e4b30a5cf1755c285e",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "96a4fcde8ad324ae72b0",
+    "url": "/monsters/static/css/main.bb0b7a51.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a4fcde8ad324ae72b0",
+    "url": "/monsters/static/js/main.cbf8f8ca.chunk.js"
+  },
+  {
+    "revision": "7023e1644df1af4b3adb",
+    "url": "/monsters/static/js/runtime-main.4abcc8fd.js"
+  }
+]);
